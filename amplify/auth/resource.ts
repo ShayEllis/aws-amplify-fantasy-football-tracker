c662d5948f9d293cb43b646dd25308145c642c42
ShayEllis <58398990+ShayEllis@users.noreply.github.com>
2024-06-19 18:35:18 -0700

    added auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,23 +4,23 @@ import { defineAuth } from '@aws-amplify/backend'
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
-// export const auth = defineAuth({
-//   loginWith: {
-//     email: {
-//       verificationEmailStyle: 'CODE',
-//       verificationEmailSubject: 'Fantasy Football Tracker - Verification Code',
-//       verificationEmailBody: (createCode) =>
-//         `Use this code to confirm your account: ${createCode()}`,
-//     },
-//   },
-//   multifactor: {
-//     mode: 'OPTIONAL',
-//     totp: true,
-//   },
-//   userAttributes: {
-//     preferredUsername: {
-//       mutable: true,
-//       required: true,
-//     },
-//   },
-// })
+export const auth = defineAuth({
+  loginWith: {
+    email: {
+      verificationEmailStyle: 'CODE',
+      verificationEmailSubject: 'Fantasy Football Tracker - Verification Code',
+      verificationEmailBody: (createCode) =>
+        `Use this code to confirm your account: ${createCode()}`,
+    },
+  },
+  multifactor: {
+    mode: 'OPTIONAL',
+    totp: true,
+  },
+  userAttributes: {
+    preferredUsername: {
+      mutable: true,
+      required: true,
+    },
+  },
+})
